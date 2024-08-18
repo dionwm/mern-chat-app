38aea9d0bfb993afd3c66c28557bc167cdc6224a
dionwm <dionwm@gmail.com>
2024-08-18 15:04:20 +0530

    Create Group Chat, RenameGroup Chat, Add a user to group, remove a user from group APIs created

--- backend/routes/chatRoute.js
+++ backend/routes/chatRoute.js
@@ -3,18 +3,25 @@ const router = express.Router();
 
 // Middleware Imports
 const { verifyAuthorization } = require("../middleware/authMiddleware");
-const { fetchChat } = require("../controllers/chatController");
+const {
+  fetchUserChat,
+  fetchAllChats,
+  createGroupChat,
+  renameGroupChat,
+  addUserToGroupChat,
+  removeUserFromGroupChat,
+} = require("../controllers/chatController");
 
 // // GET
-router.post("/", verifyAuthorization, fetchChat);
+router.get("/", verifyAuthorization, fetchAllChats);
 
 // // POST
-// router.post("/", verifyAuthorization, createChat);
-// router.post("/group", verifyAuthorization, createGroupChat);
+router.post("/", verifyAuthorization, fetchUserChat);
+router.post("/group", verifyAuthorization, createGroupChat);
 
 // // PUT
-// router.put("/group/rename", verifyAuthorization, renameGroupChat);
-// router.put("/group/add-user", verifyAuthorization, addUserToGroupChat);
-// router.put("/group/remove-user", verifyAuthorization, removeUserFromGroupChat);
+router.put("/group/rename", verifyAuthorization, renameGroupChat);
+router.put("/group/add-user", verifyAuthorization, addUserToGroupChat);
+router.put("/group/remove-user", verifyAuthorization, removeUserFromGroupChat);
 
 module.exports = router;
